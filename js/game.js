20a8517c7c0119d6d329ddd1162a66845a7e1bf4
K1llf0rce <fteppan@gmx.at>
2021-04-05 03:04:42 +0200

    make shoot more responsive (unfinished)

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
-/* 
+/*
 Last Author: K1llf0rce
-Date: 01.04.2021
+Date: 04.04.2021
 */
 
 //exec code in strict mode
@@ -22,11 +22,12 @@ let imageBullet = document.getElementById('bullet1');
 let posX = 800;
 let posY = 700;
 
-//move variables for each individual axis to increase responsiveness
-let moveU = '';
-let moveD = '';
-let moveL = '';
-let moveR = '';
+//move/shoot variables for each individual axis to increase responsiveness
+let moveU = false;
+let moveD = false;
+let moveL = false;
+let moveR = false;
+let shoot = false;
 
 //global speed adjustment
 let globalSpeed = 7;
@@ -34,6 +35,7 @@ let globalBulletSpeed = 5;
 
 //bullet array
 let array = [];
+let shootInit = false;
 
 //animation loop
 function loop() {
@@ -45,85 +47,116 @@ function loop() {
   //spaceship movement
   ctx.drawImage(image, posX, posY, 60, 60); //draws image of choice and scales it
   keepMoving();
-  
+
+  //generate bullets
+  shootCheck()
+
   //keep bullets moving
   bulletMovement();
 
   window.requestAnimationFrame(loop);
 }
 
-//adjust position of spaceship according to key events and perform out of border checks
+// adjust position of spaceship according to key events and perform out of
+// border checks
 function keepMoving() {
-  if (moveR == 'right') {
-    if ((posX + image.width +5) < cv.width) {
+  if (moveR == true) {
+    if ((posX + image.width + 5) < cv.width) {
       posX += globalSpeed;
     }
-  } else if (moveL == 'left') {
-    if ((posX + image.width -5) > image.width) {
+  } else if (moveL == true) {
+    if ((posX + image.width - 5) > image.width) {
       posX -= globalSpeed;
     }
-  } else if (moveU == 'up') {
-    if ((posY + image.height -5) > cv.height / 1.5) { //dont let spaceship move all the way up 
+  } else if (moveU == true) {
+    if ((posY + image.height - 5) > cv.height / 1.5) { //dont let spaceship move all the way up
       posY -= globalSpeed;
     }
-  } else if (moveD == 'down') {
-    if ((posY + image.height +5) < cv.height) {
+  } else if (moveD == true) {
+    if ((posY + image.height + 5) < cv.height) {
       posY += globalSpeed;
     }
   }
 }
 
+function shootCheck() {
+  if (shootInit == true) {
+    var shootInterval = setInterval(generateBullet, 500);
+    shootInit = false;
+  }
+}
+
+//generate bullet
+function generateBullet() {
+  let bl1 = new Bullet();
+  array.push(bl1);
+}
+
 //add bulet movement
 function bulletMovement() {
-  for (let i=0; i < array.length; i++) {
-    ctx.drawImage(imageBullet, array[i].initialPosX, (array[i].initialPosY+array[i].speedY), 20, 20);
+  for (let i = 0; i < array.length; i++) {
+    ctx.drawImage(
+      imageBullet,
+      array[i].initialPosX,
+      (array[i].initialPosY + array[i].speedY),
+      20,
+      20
+    );
     array[i].speedY -= globalBulletSpeed;
-    if ( (array[i].initialPosY+array[i].speedY) == 0) {
-      array.splice(i, 1); 
+    if ((array[i].initialPosY + array[i].speedY) == 0) {
+      array.splice(i, 1);
     }
   }
 }
 
 //class for bullets
 class Bullet {
-  constructor () {
-    this.initialPosX = posX+20; //shift to center
+  constructor() {
+    this.initialPosX = posX + 20; //shift to center
     this.initialPosY = posY;
     this.speedY = 0;
   }
 }
 
-//if keydown event is triggered, start moving spaceship with keepMoving-function untill keydown event triggers
+//if keydown event is triggered
 document.addEventListener('keydown', function (event) {
   if (event.code == 'ArrowUp') {
-    moveU = 'up';
+    moveU = true;
   } else if (event.code == 'ArrowDown') {
-    moveD = 'down';
+    moveD = true;
   } else if (event.code == 'ArrowLeft') {
-    moveL = 'left';
+    moveL = true;
   } else if (event.code == 'ArrowRight') {
-    moveR = 'right';
+    moveR = true;
+    clearInterval(intervalId);
   }
 });
 
-//wait for keyup event, if detected set move to "stop" so keepMoving-function doesnt continue
+//wait for keyup event
 document.addEventListener('keyup', function (event) {
   if (event.code == 'ArrowUp') {
-    moveU = '';
+    moveU = false;
   } else if (event.code == 'ArrowDown') {
-    moveD = '';
+    moveD = false;
   } else if (event.code == 'ArrowLeft') {
-    moveL = '';
+    moveL = false;
   } else if (event.code == 'ArrowRight') {
-    moveR = '';
+    moveR = false;
   }
 });
 
-//listen for shoot
+//if keydown event is triggered
+document.addEventListener('keydown', function (event) {
+  if (event.code == 'Space') {
+    shootInit = true;
+    shoot = true;
+  }
+});
+
+//wait for keyup event
 document.addEventListener('keyup', function (event) {
   if (event.code == 'Space') {
-    let bl1 = new Bullet();
-    array.push(bl1);
+    shoot = false;
   }
 });
 
